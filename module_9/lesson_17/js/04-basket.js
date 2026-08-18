9be56351ded82e32caf56116b6a014b931f0b21b
Dmitriy-mn <dmitriy.k.89.mn@gmail.com>
2026-08-18 22:02:28 +0300

    lesson_17

---
+++ module_9/lesson_17/js/04-basket.js
@@ -0,0 +1,38 @@
+const PRODUCT_LS_KEY = 'basket';
+
+const totalPrice = document.querySelector(".js-total-price");
+const clear = document.querySelector(".js-clear");
+const container = document.querySelector(".js-list");
+
+const products = JSON.parse(localStorage.getItem(PRODUCT_LS_KEY)) ?? []; 
+let totalCost;
+
+if(products.length) {
+    clear.hidden = false;
+    totalCost = products.reduce((acc, { qty, price }) => acc + (qty * price), 0);
+}
+
+totalPrice.textContent = totalCost ? `Total cost ${totalCost} грн` : "Your basket is empty";
+container.insertAdjacentHTML("beforeend", createMarkup(products));
+clear.addEventListener("click", handlerClear);
+
+function handlerClear() {
+    localStorage.removeItem(PRODUCT_LS_KEY);
+    window.location.href = "./03-shop.html";
+}
+
+function createMarkup(arr) {
+    return arr.map(({ img, name, price, qty }) => `
+        <li class="cart-item">
+            <img src="${img}" alt="${name}" class="product-img"/>
+            <h2>${name}</h2>
+            <p>Quantity: ${qty}</p>
+            <p>Total price: ${qty * price} грн</p>
+        </li>
+    `).join("");
+}
+
+
+
+
+
